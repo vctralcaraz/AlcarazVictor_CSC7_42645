905376c12900e0bc7189b4f77ea93e82b4633453
vctralcaraz <vctralcaraz@gmail.com>
2017-04-16 23:37:42 -0700

    Signed-off-by: vctralcaraz <vctralcaraz@gmail.com>

--- Project/mastermind_v3/game.cpp
+++ Project/mastermind_v3/game.cpp
@@ -11,20 +11,65 @@ Game::Game()
 {
     size = 4;
     nTry = 10;
+    nGuess = 0;
     
-    initializing 2D array
-    *score = new int[nTry];
+    // initializing 2D arrays
+//    cout << "Initializing "2D" arrays" << endl;
+    table = new string[nTry];
+    score = new int*[nTry];
+    
+    // adding placeholders for the table of guess
+    string temp = "";
+    
+    for(int i = 0; i < size; i++)
+        temp += "_ ";
+    
+    // initialing all scores to zero
     for(int i = 0; i < nTry; i++)
-        score[i] = 0;
+        score[i] = new int[2];
     
-    encrypt();
-    prntCde();
-    guess();
+    for(int i = 0; i < nTry; i++)
+        for(int j = 0; j < 2; j++)
+            score[i][j] = 0;
+    
+//    cout << temp << endl;
+    for(int i = 0; i < nTry; i++)
+        table[i] = temp;
+    
+    // play the game
+    plyGame();
 }
 
 Game::~Game()
 {
+    delete []table;
+}
+
+// Play the game
+void Game::plyGame()
+{
+    bool gameOver = false;
     
+    // encrypt the code
+    encrypt();
+    
+    // play the game while a winning or losing condition isn't met
+    while(!gameOver)
+    {
+        // print the table
+        prntTbl();
+        
+        // *TEMP* print the code
+        prntCde();
+        
+        // user guesses
+        // return validated guess
+        gameOver = guess();
+        
+        if(!gameOver && (nGuess + 1) > nTry) gameOver = true; 
+        
+        if(gameOver) prntTbl();
+    }
 }
 
 // randomize a code
@@ -46,19 +91,105 @@ void Game::prntCde()
 }
 
 // user's guess
-void Game::guess()
+bool Game::guess()
 {
+    // prompt user guess
     string userG;
     
     cout << "What is your guess? ";
     getline(cin, userG);
-    
-    vldte(userG);
+    nGuess++;
+    addGess(userG);
+    // validate user guess
+    return vldte(userG);
 }
 
 // validation for the user guess
-void Game::vldte(string g)
+bool Game::vldte(string g)
 {
-    if(g == code) cout << "Correct!" << endl;
-    else cout << "Incorrect." << endl;
+    // initialize a boolean check array
+    bool *check = new bool[size];
+    
+    for(int i = 0; i < size; i++)
+        check[i] = false;
+    
+    // initialize an array of size of 2
+    int *lineScr = new int[2];
+    
+    for(int i = 0; i < 2; i++)
+        lineScr[i] = 0;
+    
+    // if the guess matches the code, then output correct
+    if(g == code)
+    {
+        cout << "Correct!" << endl;
+        lineScr[1] = 4;
+        addScre(lineScr);
+        
+        delete [] lineScr;
+        delete [] check;
+        return true; // game is over
+    }
+    
+    for(int i = 0; i < size; i++)
+    {
+        // check the number if it's in the same index
+        if(!check[i] && g[i] == code[i])
+        {
+            lineScr[1]++;
+            check[i] = true;
+        }
+        else    // check the number if it exists in the code
+        {
+            for(int j = 0; j < size; j++)
+            {
+                if(!check[j] && g[i] == code[j])
+                {
+                    lineScr[0]++;
+                    check[j] = true;
+                    break;
+                }
+            }
+        }
+    }
+    
+    addScre(lineScr);
+    
+    delete [] lineScr;
+    delete [] check;
+    return false;
+}
+
+// print the table
+void Game::prntTbl()
+{
+    // print table by line
+    for(int i = 0; i < nTry; i++)
+    {
+        cout << table[i] << "|| ";
+        for(int j = 0; j < 2; j++)
+            cout << score[i][j] << ' ';
+        
+        cout << endl;
+    }
+}
+
+// add the guess to the table
+void Game::addGess(string g)
+{
+    int temp = nTry - nGuess;
+    table[temp] = "";
+    for(int i = 0; i < size; i++)
+    {
+        table[temp] += g[i];
+        table[temp] += ' ';
+    }
+}
+
+// add the guesses score to the score array
+void Game::addScre(int *s)
+{
+    int temp = nTry - nGuess;
+    for(int i = 0; i < 2; i++)
+        score[temp][i] = s[i];
 }
--- Project/mastermind_v3/game.h
+++ Project/mastermind_v3/game.h
@@ -17,18 +17,23 @@ class Game {
 private:
     string code;    // The code to be broken
     int size;       // size of the code
-    int **score;    // 2D array that holds the current guess' score
+    string *table;  // "2D" array that holds the table of guesses
+    int **score;    // 2D array that holds the guesses score
     int nTry;       // number of tries in the game
-    int uGuess;     // array to hold user guess
+    int nGuess;     // guess number
     
 public:
     Game();     // constructor
     ~Game();    // destructor
     
-    void encrypt();     // code making
-    void prntCde();     // code printing
-    void guess();       // user input (guess)
-    void vldte(string); // validate the guess
+    void encrypt();         // code making
+    void prntCde();         // code printing
+    void prntTbl();         // table printing
+    bool guess();           // user input (guess)
+    bool vldte(string);     // validate the guess
+    void addGess(string);   // add guess to the table
+    void addScre(int *);    // add the guesses score to the score array
+    void plyGame();         // play the game
 };
 
 #endif /* GAME_H */
